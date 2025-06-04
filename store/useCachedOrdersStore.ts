601bb7a755aad10015bdf7ba4f1fd1a4f1ba8aa0
Comercialsc7 <comercial.sc7@dmuller.com.br>
2025-06-04 10:10:48 -0300

    Update sync orders functionality

--- store/useCachedOrdersStore.ts
+++ store/useCachedOrdersStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
-import { OrderItem, Client, PaymentTerm } from './useOrderStore'; // Importa as definições necessárias
+import { OrderItem, Client, PaymentTerm } from './useOrderStore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export interface CachedOrder {
@@ -17,23 +17,22 @@ export interface CachedOrder {
     description: string;
     photo?: string;
   };
-  // Adicionar outros campos se necessário
 }
 
 interface CachedOrdersState {
-  cachedOrders: CachedOrder[]; // Usar a nova interface CachedOrder
-  _hasHydrated: boolean; // Adicionar estado para controle de hidratação
-  addCachedOrder: (order: CachedOrder) => void; // Aceitar CachedOrder
+  cachedOrders: CachedOrder[];
+  _hasHydrated: boolean;
+  addCachedOrder: (order: CachedOrder) => void;
   clearCachedOrders: () => void;
   getOrderById: (id: string) => CachedOrder | undefined;
-  setHasHydrated: (state: boolean) => void; // Adicionar ação para atualizar o estado de hidratação
+  setHasHydrated: (state: boolean) => void;
 }
 
 export const useCachedOrdersStore = create<CachedOrdersState>()(
   persist(
     (set, get) => ({
       cachedOrders: [],
-      _hasHydrated: false, // Inicialmente false
+      _hasHydrated: false,
       addCachedOrder: (order) => set((state) => ({
         cachedOrders: [...state.cachedOrders, order],
       })),
@@ -43,22 +42,20 @@ export const useCachedOrdersStore = create<CachedOrdersState>()(
         set({
           _hasHydrated: state
         });
-      }, // Implementação da ação
+      },
     }),
     {
       name: 'cached-orders-storage',
-      storage: createJSONStorage(() => AsyncStorage as any), // Usar AsyncStorage
+      storage: createJSONStorage(() => AsyncStorage as any),
       onRehydrateStorage: (state) => {
-        console.log('cached-orders-storage hydration starts');
         return (state, error) => {
           if (error) {
             console.error('cached-orders-storage hydration failed', error);
           } else {
-            console.log('cached-orders-storage hydration finished');
             (state as CachedOrdersState).setHasHydrated(true);
           }
         };
       },
     }
   )
-); 
+);
