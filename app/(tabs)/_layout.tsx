c8d282a51f69c34dc0c2cc6852b94817ab1917e8
Comercialsc7 <comercial.sc7@dmuller.com.br>
2025-06-15 19:35:08 -0300

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, Users, Package, Settings } from 'lucide-react-native';
+import { Chrome as Home, Users, Package, Settings } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
